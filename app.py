0bcdd0a84d03a3a5c74aec7c7fdd001e86204c69
Vadim Sohin , DevOps  Engineer <vadim.sohin@gmail.com>
2021-10-27 19:54:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,3 +10,6 @@ app = Flask(__name__)
 @app.route("/get_my_ip", methods=["GET"])
 def get_my_ip():
     return jsonify({'ip': request.remote_addr}), 200
+
+
+# run the command gunicorn app:app -w 4 --threads 2 -b X.X.X.X:5000
